d11e1a84729a815b54f3278cf9cfbdb738830e3b
Joseph Miller <jdworldtv@gmail.com>
2019-10-30 03:12:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-array_each_with.index do techno rap rock regga
+array.each_with.index do techno rap rock regga
  
 
  end
